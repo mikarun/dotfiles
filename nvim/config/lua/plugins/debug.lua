2c8f764481c7e3585193b4dcae8d04b2bf089c34
Mickael Gerard <mikarun@gmail.com>
2025-01-24 08:38:12 +0100

    remove dap-ui for dap-view

--- nvim/config/lua/plugins/debug.lua
+++ nvim/config/lua/plugins/debug.lua
@@ -12,10 +12,7 @@ return {
 	-- NOTE: And you can specify dependencies as well
 	dependencies = {
 		-- Creates a beautiful debugger UI
-		"rcarriga/nvim-dap-ui",
-
-		-- Required dependency for nvim-dap-ui
-		"nvim-neotest/nvim-nio",
+		{ "igorlfs/nvim-dap-view", opts = {} },
 
 		-- Installs the debug adapters for you
 		"williamboman/mason.nvim",
@@ -74,32 +71,15 @@ return {
 		{
 			"<F7>",
 			function()
-				require("dapui").toggle()
+				require("dap-view").toggle()
 			end,
-			desc = "Debug: See last session result.",
-		},
-		{
-			"<leader>duo",
-			function()
-				require("dapui").open()
-			end,
-		},
-		{
-			"<leader>duc",
-			function()
-				require("dapui").close()
-			end,
-		},
-		{
-			"<leader>dr",
-			function()
-				require("dap").repl.open()
-			end,
+			desc = "Toggle dap view",
 		},
 	},
 	config = function()
 		local dap = require("dap")
-		local dapui = require("dapui")
+
+		require("mason").setup()
 
 		require("mason-nvim-dap").setup({
 			-- Makes a best effort to setup the various debuggers with
@@ -118,28 +98,6 @@ return {
 			},
 		})
 
-		-- Dap UI setup
-		-- For more information, see |:help nvim-dap-ui|
-		dapui.setup({
-			-- Set icons to characters that are more likely to work in every terminal.
-			--    Feel free to remove or use ones that you like more! :)
-			--    Don't feel like these are good choices.
-			icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-			controls = {
-				icons = {
-					pause = "⏸",
-					play = "▶",
-					step_into = "⏎",
-					step_over = "⏭",
-					step_out = "⏮",
-					step_back = "b",
-					run_last = "▶▶",
-					terminate = "⏹",
-					disconnect = "⏏",
-				},
-			},
-		})
-
 		-- Change breakpoint icons
 		-- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
 		-- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
@@ -152,9 +110,9 @@ return {
 		--   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
 		-- end
 
-		dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-		dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-		dap.listeners.before.event_exited["dapui_config"] = dapui.close
+		--dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+		--dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+		--dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
 		dap.adapters.php = {
 			type = "executable",
