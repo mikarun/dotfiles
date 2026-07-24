3e74db757c42225d2c99c65cda8445a8348ed13b
Mickael Gerard <mikarun@gmail.com>
2026-07-24 13:48:03 +0200

    disable install color-scheme mocha

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -43,6 +43,7 @@ return {
   -- change some telescope options and a keymap to browse plugin files
   {
     "nvim-telescope/telescope.nvim",
+    enabled = false,
     keys = {
       -- add a keymap to browse plugin files
       -- stylua: ignore
